1db53717961b1c2ef017f1787dbcda555e80e468
Johannes Gräger <jgraeger@posteo.de>
2022-05-06 14:54:19 +0200

    Made gke cluster private

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_service_account" "default" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
+  name     = "k8s-gke-cluster"
   location = var.region
 
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -12,10 +12,31 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  # Settings for making the cluster private and vpc-native
+  network         = google_compute_network.vpc_network.self_link
+  subnetwork      = google_compute_subnetwork.vpc_subnet.self_link
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {
+    # TODO: Don't hardcode names here
+    cluster_secondary_range_name  = "vpc-pod-range"
+    services_secondary_range_name = "vpc-service-range"
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+
+  depends_on = [
+    google_compute_subnetwork.vpc_subnet
+  ]
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
+  name       = "k8s-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = 1
@@ -26,7 +47,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
--- vpc.tf
+++ vpc.tf
@@ -1,50 +1,58 @@
 locals {
-    k8s_node_subnet = "10.10.0.0/16"
-    k8s_pod_subnet = "10.11.0.0/16"
-    k8s_service_subnet = "10.12.0.0/16"
+  k8s_node_subnet    = "10.10.0.0/28"
+  k8s_pod_subnet     = "10.11.0.0/16"
+  k8s_service_subnet = "10.12.0.0/16"
 }
 
 resource "google_compute_network" "vpc_network" {
-  project                 = var.project_name
-  name                    = "kubernetes-vpc"
-  auto_create_subnetworks = false
+  project                         = var.project_name
+  name                            = "kubernetes-vpc"
+  auto_create_subnetworks         = false
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "vpc_subnet" {
-  name = "kubernetes-vpc-subnet"
-  ip_cidr_range = local.k8s_node_subnet
-  region = var.region
-  network = google_compute_network.vpc_network.id
+  name                     = "kubernetes-vpc-subnet"
+  ip_cidr_range            = local.k8s_node_subnet
+  region                   = var.region
+  network                  = google_compute_network.vpc_network.id
+  private_ip_google_access = true
   secondary_ip_range {
-      range_name = "vpc-pod-range"
-      ip_cidr_range = local.k8s_pod_subnet
+    range_name    = "vpc-pod-range"
+    ip_cidr_range = local.k8s_pod_subnet
   }
   secondary_ip_range {
-      range_name = "vpc-service-range"
-      ip_cidr_range = local.k8s_service_subnet
+    range_name    = "vpc-service-range"
+    ip_cidr_range = local.k8s_service_subnet
   }
 }
 
+resource "google_compute_route" "egress_internet" {
+  name             = "vpc-egress-internet"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc_network.name
+  next_hop_gateway = "default-internet-gateway"
+}
+
 resource "google_compute_router" "vpc_router" {
-  name = "kubernetes-vpc-router"
-  region = var.region
+  name    = "kubernetes-vpc-router"
+  region  = var.region
   project = var.project_name
   network = google_compute_network.vpc_network.name
 }
 
 resource "google_compute_router_nat" "vpc_nat" {
-  name = "kubernetes-vpc-nat"
+  name    = "kubernetes-vpc-nat"
   project = var.project_name
-  router = google_compute_router.vpc_router.name
-  region = var.region
+  router  = google_compute_router.vpc_router.name
+  region  = var.region
 
-  nat_ip_allocate_option = "AUTO_ONLY"
+  nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
 
   subnetwork {
-    name = google_compute_subnetwork.vpc_subnet.name
-    source_ip_ranges_to_nat = [ "ALL_IP_RANGES" ]
+    name                    = google_compute_subnetwork.vpc_subnet.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 
   log_config {
