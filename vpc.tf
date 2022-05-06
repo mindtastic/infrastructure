00e03ef7ba9dc12a320664681e6372e8152dabce
Johannes Gräger <jgraeger@posteo.de>
2022-05-06 09:40:07 +0200

    Added router and nat

--- vpc.tf
+++ vpc.tf
@@ -25,3 +25,30 @@ resource "google_compute_subnetwork" "vpc_subnet" {
       ip_cidr_range = local.k8s_service_subnet
   }
 }
+
+resource "google_compute_router" "vpc_router" {
+  name = "kubernetes-vpc-router"
+  region = var.region
+  project = var.project_name
+  network = google_compute_network.vpc_network.name
+}
+
+resource "google_compute_router_nat" "vpc_nat" {
+  name = "kubernetes-vpc-nat"
+  project = var.project_name
+  router = google_compute_router.vpc_router.name
+  region = var.region
+
+  nat_ip_allocate_option = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name = google_compute_subnetwork.vpc_subnet.name
+    source_ip_ranges_to_nat = [ "ALL_IP_RANGES" ]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
