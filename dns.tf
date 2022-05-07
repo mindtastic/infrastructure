e4e2330f575bb90876e6803ea833a93c22eae17f
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-07 17:33:24 +0200

    Add subzone to DNS

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_record" "mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.mindtastic_lol.name_servers)
+  for_each = toset(google_dns_managed_zone.root.name_servers)
 
   zone_id = var.cloudflare_zone_id
   name    = "@"
@@ -8,9 +8,24 @@ resource "cloudflare_record" "mindtastic_lol" {
   ttl     = 3600
 }
 
-
-resource "google_dns_managed_zone" "mindtastic_lol" {
+resource "google_dns_managed_zone" "root" {
   name        = "mindtastic-root-zone"
   dns_name    = "mindtastic.lol."
   description = "The base DNS zone, delegated from Cloudflare"
+}
+
+resource "google_dns_managed_zone" "live" {
+  name        = "mindtastic-live-zone"
+  dns_name    = "live.mindtastic.lol."
+  description = "The base DNS zone for live environment"
+}
+
+resource "google_dns_record_set" "live" {
+  name = "live.${google_dns_managed_zone.root.dns_name}"
+  type = "NS"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.root.name
+
+  rrdatas = [toset(google_dns_managed_zone.live.name_servers)]
 }
