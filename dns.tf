50e1dc4c3f1fc8cca065390c49ddf37f64783e9a
Johannes Gräger <jgraeger@posteo.de>
2022-05-07 18:23:25 +0200

    Removed live zone record in google dns zone

--- dns.tf
+++ dns.tf
@@ -1,15 +1,15 @@
-resource "cloudflare_record" "mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.live.name_servers)
-
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  value   = each.key
-  type    = "NS"
-  ttl     = 3600
-}
-
 resource "google_dns_managed_zone" "live" {
   name        = "mindtastic-live-zone"
   dns_name    = "live.mindtastic.lol."
   description = "The base DNS zone for live environment"
+}
+
+resource "cloudflare_record" "live_mindtastic_lol" {
+  for_each = toset(google_dns_managed_zone.live.name_servers)
+
+  zone_id = var.cloudflare_zone_id
+  name    = "live"
+  value   = each.key
+  type    = "NS"
+  ttl     = 3600
 }
