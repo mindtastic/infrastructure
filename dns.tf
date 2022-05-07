553802c172702292f69075cf93b2d922831bfa52
Leo <10501668+ldb@users.noreply.github.com>
2022-05-07 17:39:21 +0200

    Add subzone to DNS (#15)
    * Add subzone to DNS
    
    * Add subzone to DNS
    
    * undo renaming
    
    * fmt

--- dns.tf
+++ dns.tf
@@ -8,9 +8,22 @@ resource "cloudflare_record" "mindtastic_lol" {
   ttl     = 3600
 }
 
-
 resource "google_dns_managed_zone" "mindtastic_lol" {
   name        = "mindtastic-root-zone"
   dns_name    = "mindtastic.lol."
   description = "The base DNS zone, delegated from Cloudflare"
+}
+
+resource "google_dns_managed_zone" "live" {
+  name        = "mindtastic-live-zone"
+  dns_name    = "live.mindtastic.lol."
+  description = "The base DNS zone for live environment"
+}
+
+resource "google_dns_record_set" "live" {
+  name         = "live.${google_dns_managed_zone.mindtastic_lol.dns_name}"
+  type         = "NS"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.mindtastic_lol.name
+  rrdatas      = toset(google_dns_managed_zone.live.name_servers)
 }
