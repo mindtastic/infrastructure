53e95d4d03cbce69a75a20ab6c41238128032ecb
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-07 17:36:48 +0200

    undo renaming

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_record" "mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.root.name_servers)
+  for_each = toset(google_dns_managed_zone.mindtastic_lol.name_servers)
 
   zone_id = var.cloudflare_zone_id
   name    = "@"
@@ -8,7 +8,7 @@ resource "cloudflare_record" "mindtastic_lol" {
   ttl     = 3600
 }
 
-resource "google_dns_managed_zone" "root" {
+resource "google_dns_managed_zone" "mindtastic_lol" {
   name        = "mindtastic-root-zone"
   dns_name    = "mindtastic.lol."
   description = "The base DNS zone, delegated from Cloudflare"
@@ -21,11 +21,11 @@ resource "google_dns_managed_zone" "live" {
 }
 
 resource "google_dns_record_set" "live" {
-  name = "live.${google_dns_managed_zone.root.dns_name}"
+  name = "live.${google_dns_managed_zone.mindtastic_lol.dns_name}"
   type = "NS"
   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.root.name
+  managed_zone = google_dns_managed_zone.mindtastic_lol.name
 
   rrdatas = toset(google_dns_managed_zone.live.name_servers)
 }
