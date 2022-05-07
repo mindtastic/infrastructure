62828ca49773b415f0009d876d380f34a1b768e8
Johannes Gräger <jgraeger@posteo.de>
2022-05-07 18:20:39 +0200

    Subzone delegation

--- dns.tf
+++ dns.tf
@@ -1,17 +1,11 @@
 resource "cloudflare_record" "mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.mindtastic_lol.name_servers)
+  for_each = toset(google_dns_managed_zone.live.name_servers)
 
   zone_id = var.cloudflare_zone_id
   name    = "@"
   value   = each.key
   type    = "NS"
   ttl     = 3600
-}
-
-resource "google_dns_managed_zone" "mindtastic_lol" {
-  name        = "mindtastic-root-zone"
-  dns_name    = "mindtastic.lol."
-  description = "The base DNS zone, delegated from Cloudflare"
 }
 
 resource "google_dns_managed_zone" "live" {
