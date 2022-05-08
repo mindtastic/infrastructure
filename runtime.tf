b2a0332ef099d0825749675ed0eab195ce5cc20e
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-08 22:37:53 +0200

    fmt

--- runtime.tf
+++ runtime.tf
@@ -2,6 +2,7 @@ module "teleport" {
   source = "./modules/teleport"
 
   cluster_name                  = var.cluster_name
+  teleport_acme_email           = var.teleport_acme_email
   teleport_github_client_id     = var.teleport_github_client_id
   teleport_github_client_secret = var.teleport_github_client_secret
   teleport_domain               = var.teleport_domain
