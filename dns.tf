91400dc39e7faddfe5d43a0bfa39319e1740e8c4
Johannes Gräger <jograeger@googlemail.com>
2022-05-08 14:05:42 +0200

    Using count metaargument for dns creation (#21)
    * Using count metaargument for dns creation
    
    * Update dns.tf
    
    Co-authored-by: Leo <10501668+ldb@users.noreply.github.com>
    
    Co-authored-by: Johannes Gräger <jgraeger@posteo.de>
    Co-authored-by: Leo <10501668+ldb@users.noreply.github.com>

--- dns.tf
+++ dns.tf
@@ -5,11 +5,16 @@ resource "google_dns_managed_zone" "live" {
 }
 
 resource "cloudflare_record" "live_mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.live.name_servers)
+  # This is a workaround to a problem rooted deeply within Terraform itself:
+  # https://github.com/hashicorp/terraform/issues/30937
+  # Since Terraform does not know the length of
+  # `google_dns_managed_zone.live.name_servers` at plan time, we hardcode the number of servers here.
+  # This works until it won't. If Google returns fewer servers, apply will fail. If Google returns more, we ignore them.
+  count = 4
 
   zone_id = var.cloudflare_zone_id
   name    = "live"
-  value   = each.key
+  value   = google_dns_managed_zone.live.name_servers[count.index]
   type    = "NS"
   ttl     = 3600
 }
