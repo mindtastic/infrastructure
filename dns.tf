7d1805ea5128c5a31f35f31b71ed6cdf4a3287f3
Johannes Gräger <jgraeger@posteo.de>
2022-05-08 09:55:22 +0200

    Using count metaargument for dns creation

--- dns.tf
+++ dns.tf
@@ -5,11 +5,12 @@ resource "google_dns_managed_zone" "live" {
 }
 
 resource "cloudflare_record" "live_mindtastic_lol" {
-  for_each = toset(google_dns_managed_zone.live.name_servers)
+  # Google cloud returns four nameservers for each dns managed zone
+  count = 4
 
   zone_id = var.cloudflare_zone_id
   name    = "live"
-  value   = each.key
+  value   = google_dns_managed_zone.live.name_servers[count.index]
   type    = "NS"
   ttl     = 3600
 }
