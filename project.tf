cef24441ba3fbeb27f2ed77e15ce139977017e29
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-16 19:55:19 +0200

    Capture Project Settings in Code

---
+++ project.tf
@@ -0,0 +1,23 @@
+locals {
+  services = [
+    "container.googleapis.com",
+    "dns.googleapis.com",
+    "iam.googleapis.com",
+    "artifactregistry.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "project" {
+  for_each = local.services
+
+  project = var.project_name
+  service = each.value
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_on_destroy         = true
+  disable_dependent_services = true
+}
