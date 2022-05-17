f3ea9d18d5df0c12184f27ed317569b704a60133
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-17 19:26:49 +0200

    deploy agent dns

--- dns.tf
+++ dns.tf
@@ -37,3 +37,12 @@ resource "cloudflare_record" "teleport_live_public" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "teleport_live_public" {
+  zone_id = var.cloudflare_zone_id
+  name    = "*.teleport"
+  value   = google_dns_record_set.teleport_internal.name
+  type    = "CNAME"
+  ttl     = 1
+}
+
