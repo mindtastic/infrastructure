2700e02d34cb29648b26e27b52dd4e06c9da50a0
Leo <10501668+ldb@users.noreply.github.com>
2022-05-17 18:54:46 +0200

    Deploy Teleport Agent (#38)
    * add agent chart
    
    * deploy agent
    
    * deploy agent

--- modules/teleport/helm.tf
+++ modules/teleport/helm.tf
@@ -13,13 +13,13 @@ resource "helm_release" "teleport" {
   name             = local.teleport_release_name
   repository       = "https://charts.releases.teleport.dev"
   chart            = "teleport-cluster"
-  version          = "9.2.1"
+  version          = "9.2.3"
   create_namespace = true
   namespace        = local.teleport_namespace
   max_history      = 3
 
   values = [
-    "${file("${path.module}/values.yaml")}"
+    "${file("${path.module}/values-cluster.yaml")}"
   ]
 
   set {
@@ -42,9 +42,11 @@ resource "helm_release" "teleport" {
   ]
 }
 
+resource "random_uuid" "teleport_node_join_token" {}
+
 resource "kubernetes_config_map" "github" {
   metadata {
-    name      = "teleport-github-config"
+    name      = local.teleport_release_name
     namespace = local.teleport_namespace
   }
 
@@ -55,6 +57,12 @@ resource "kubernetes_config_map" "github" {
       teleport_github_client_secret = var.teleport_github_client_secret
       teleport_domain               = var.teleport_domain
       teleport_github_org           = var.teleport_github_org
+    })}",
+    "teleport.yaml" = "${templatefile("${path.module}/teleport.yaml", {
+      teleport_domain          = var.teleport_domain,
+      teleport_node_join_token = random_uuid.teleport_node_join_token.result,
+      cluster_name             = var.cluster_name,
+      teleport_acme_email      = var.teleport_acme_email,
     })}"
   }
 
@@ -70,4 +78,33 @@ data "kubernetes_service" "teleport" {
   }
 
   depends_on = [helm_release.teleport]
+}
+
+resource "helm_release" "teleport_agent" {
+  name             = local.teleport_release_name
+  repository       = "https://charts.releases.teleport.dev"
+  chart            = "teleport-agent"
+  version          = "9.2.3"
+  create_namespace = true
+  namespace        = local.teleport_namespace
+  max_history      = 3
+
+  values = [
+    "${file("${path.module}/values-agent.yaml")}"
+  ]
+
+  set_sensitive {
+    name  = "authToken"
+    value = random_uuid.teleport_node_join_token.result
+  }
+
+  set {
+    name  = "proxyAddr"
+    value = "${var.teleport_domain}:443"
+  }
+
+  set {
+    name  = "kubeClusterName"
+    value = var.cluster_name
+  }
 }
