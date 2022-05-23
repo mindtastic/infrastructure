c1d5662afe6589a0194849e09a56a7c194d42e46
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-23 21:43:52 +0200

    Initial Commit

--- dns.tf
+++ dns.tf
@@ -60,3 +60,27 @@ resource "cloudflare_record" "live_mindtastic_lol" {
   ]
 }
 
+# # Teleport
+# resource "google_dns_record_set" "teleport_internal" {
+#   name         = "teleport.net.${google_dns_managed_zone.live.dns_name}"
+#   type         = "A"
+#   ttl          = 300
+#   managed_zone = google_dns_managed_zone.live.name
+#   rrdatas      = [module.teleport.teleport_public_ip]
+# }
+
+# resource "cloudflare_record" "teleport_live_public" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "teleport"
+#   value   = google_dns_record_set.teleport_internal.name
+#   type    = "CNAME"
+#   ttl     = 1
+# }
+
+# resource "cloudflare_record" "teleport_live_public_wildcard" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "*.teleport"
+#   value   = google_dns_record_set.teleport_internal.name
+#   type    = "CNAME"
+#   ttl     = 1
+# }
