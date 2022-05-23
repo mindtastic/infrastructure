3c8c96fb7bca440aec98e1a131365c0540a67135
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-23 19:51:01 +0200

    Initial Commit

--- peerings/peerings.tf
+++ peerings/peerings.tf
@@ -15,13 +15,13 @@ data "tfe_outputs" "live" {
 
 # DEV <-> LIVE
 
-resource "google_compute_network_peering" "peering1" {
+resource "google_compute_network_peering" "dev-live" {
   name         = "dev-live"
   network      = data.tfe_outputs.dev.vpc_self_link
   peer_network = data.tfe_outputs.live.vpc_self_link
 }
 
-resource "google_compute_network_peering" "peering1" {
+resource "google_compute_network_peering" "live-dev" {
   name         = "live-dev"
   network      = data.tfe_outputs.live.vpc_self_link
   peer_network = data.tfe_outputs.dev.vpc_self_link
@@ -29,13 +29,13 @@ resource "google_compute_network_peering" "peering1" {
 
 # STAGE <-> LIVE
 
-resource "google_compute_network_peering" "peering1" {
+resource "google_compute_network_peering" "stage-live" {
   name         = "stage-live"
   network      = data.tfe_outputs.stage.vpc_self_link
   peer_network = data.tfe_outputs.live.vpc_self_link
 }
 
-resource "google_compute_network_peering" "peering1" {
+resource "google_compute_network_peering" "live-stage" {
   name         = "live-stage"
   network      = data.tfe_outputs.live.vpc_self_link
   peer_network = data.tfe_outputs.stage.vpc_self_link
