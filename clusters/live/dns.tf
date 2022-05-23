11011309a771cf1156b5c79917f65cae48e33dfa
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-23 21:47:57 +0200

    Initial Commit

--- clusters/live/dns.tf
+++ clusters/live/dns.tf
@@ -3,3 +3,12 @@ resource "google_dns_managed_zone" "root" {
   dns_name    = "${var.environment}.mindtastic.lol."
   description = "The base DNS zone for ${var.environment} environment"
 }
+
+# Teleport
+resource "google_dns_record_set" "teleport_internal" {
+  name         = "teleport.net.${google_dns_managed_zone.root.dns_name}"
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.root.name
+  rrdatas      = [module.teleport.teleport_public_ip]
+}
--- dns.tf
+++ dns.tf
@@ -60,14 +60,7 @@ resource "cloudflare_record" "live_mindtastic_lol" {
   ]
 }
 
-# # Teleport
-# resource "google_dns_record_set" "teleport_internal" {
-#   name         = "teleport.net.${google_dns_managed_zone.live.dns_name}"
-#   type         = "A"
-#   ttl          = 300
-#   managed_zone = google_dns_managed_zone.live.name
-#   rrdatas      = [module.teleport.teleport_public_ip]
-# }
+# # Public DNS
 
 # resource "cloudflare_record" "teleport_live_public" {
 #   zone_id = var.cloudflare_zone_id
