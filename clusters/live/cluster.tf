bc156ee37d18ef9b0ba1ed470c8645af10df23c6
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-23 22:00:06 +0200

    Initial Commit

--- clusters/live/cluster.tf
+++ clusters/live/cluster.tf
@@ -42,7 +42,7 @@ resource "google_container_cluster" "primary" {
 
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block   = local.tfc_runner_ip
+      cidr_block   = "0.0.0.0/0" # local.tfc_runner_ip
       display_name = "Terraform Cloud Runner"
     }
   }
