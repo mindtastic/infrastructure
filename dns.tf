eafb70ca1606b485ea89c161e916d87ab1e30c14
Isabel Schmuck <isabel.schmuck@dampsoft.de>
2022-05-24 14:08:21 +0200

    Add period to cloudfare record value

--- dns.tf
+++ dns.tf
@@ -15,7 +15,7 @@ resource "cloudflare_record" "live_mindtastic_lol" {
 
   zone_id = var.cloudflare_zone_id
   name    = "live"
-  value   = google_dns_managed_zone.live.name_servers[count.index]
+  value   = "${google_dns_managed_zone.live.name_servers[count.index]}."
   type    = "NS"
   ttl     = 3600
 }
