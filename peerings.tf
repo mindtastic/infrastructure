9b60dcb2e0ada91be704acfc4f583058b679bb93
Johannes Gräger <jgraeger@posteo.de>
2022-05-25 17:05:22 +0200

    Removed inter-cluster peerings in root workspace

--- peerings.tf
+++ peerings.tf
@@ -1,27 +1,31 @@
-# DEV <-> LIVE
+#
+# TODO: Check if we need the peerings later.
+#
 
-resource "google_compute_network_peering" "dev-live" {
-  name         = "dev-live"
-  network      = data.tfe_outputs.dev.values.vpc_self_link
-  peer_network = data.tfe_outputs.live.values.vpc_self_link
-}
+# # DEV <-> LIVE
 
-resource "google_compute_network_peering" "live-dev" {
-  name         = "live-dev"
-  network      = data.tfe_outputs.live.values.vpc_self_link
-  peer_network = data.tfe_outputs.dev.values.vpc_self_link
-}
+# resource "google_compute_network_peering" "dev-live" {
+#   name         = "dev-live"
+#   network      = data.tfe_outputs.dev.values.vpc_self_link
+#   peer_network = data.tfe_outputs.live.values.vpc_self_link
+# }
 
-# STAGE <-> LIVE
+# resource "google_compute_network_peering" "live-dev" {
+#   name         = "live-dev"
+#   network      = data.tfe_outputs.live.values.vpc_self_link
+#   peer_network = data.tfe_outputs.dev.values.vpc_self_link
+# }
 
-resource "google_compute_network_peering" "stage-live" {
-  name         = "stage-live"
-  network      = data.tfe_outputs.stage.values.vpc_self_link
-  peer_network = data.tfe_outputs.live.values.vpc_self_link
-}
+# # STAGE <-> LIVE
 
-resource "google_compute_network_peering" "live-stage" {
-  name         = "live-stage"
-  network      = data.tfe_outputs.live.values.vpc_self_link
-  peer_network = data.tfe_outputs.stage.values.vpc_self_link
-}
+# resource "google_compute_network_peering" "stage-live" {
+#   name         = "stage-live"
+#   network      = data.tfe_outputs.stage.values.vpc_self_link
+#   peer_network = data.tfe_outputs.live.values.vpc_self_link
+# }
+
+# resource "google_compute_network_peering" "live-stage" {
+#   name         = "live-stage"
+#   network      = data.tfe_outputs.live.values.vpc_self_link
+#   peer_network = data.tfe_outputs.stage.values.vpc_self_link
+# }
