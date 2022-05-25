e9514d9ad2898677cfea18aa2362d79ef56e9b88
Johannes Gräger <jgraeger@posteo.de>
2022-05-25 11:30:31 +0200

    Specified cluster module output dependencies

--- clusters/dev/output.tf
+++ clusters/dev/output.tf
@@ -1,9 +1,17 @@
 output "dns_zone_names" {
   description = "Name Servers for the root DNS zone in this environment"
   value       = google_dns_managed_zone.root.name_servers
+
+  depends_on = [
+    google_dns_managed_zone.root
+  ]
 }
 
 output "vpc_self_link" {
   description = "Self Link of VPC for this Cluster"
   value       = google_compute_network.vpc_network.self_link
+
+  depends_on = [
+    google_compute_network.vpc_network
+  ]
 }
