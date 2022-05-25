6e5275ed90bc219d0f75b365538e10fbea8b078f
Johannes Gräger <jgraeger@posteo.de>
2022-05-25 11:19:17 +0200

    Defer reading workspace data until creation

--- datasources.tf
+++ datasources.tf
@@ -1,14 +1,26 @@
 data "tfe_outputs" "dev" {
   organization = local.tfe_organization
   workspace    = local.dev_workspace_name
+
+  depends_on = [
+    module.cluster_dev
+  ]
 }
 
 data "tfe_outputs" "stage" {
   organization = local.tfe_organization
   workspace    = local.stage_workspace_name
+
+  depends_on = [
+    module.cluster_stage
+  ]
 }
 
 data "tfe_outputs" "live" {
   organization = local.tfe_organization
   workspace    = local.live_workspace_name
+
+  depends_on = [
+    module.cluster_live
+  ]
 }
