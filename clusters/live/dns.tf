24ddf0e96c6ccff15e02be08bbc79442f87b4aec
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 13:15:14 +0200

    data provider dependency

--- clusters/live/dns.tf
+++ clusters/live/dns.tf
@@ -5,4 +5,8 @@ resource "google_dns_record_set" "teleport_internal" {
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.cluster_root.name
   rrdatas      = [module.teleport.teleport_public_ip]
+
+  depends_on = [
+    data.google_dns_managed_zone.cluster_root
+  ]
 }
