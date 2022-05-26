1d8a007ac08885383bfa5bc07f34dd6ceb1f5140
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 17:38:10 +0200

    Fixed wrong module call for teleport-agent

--- clusters/stage/runtime.tf
+++ clusters/stage/runtime.tf
@@ -1,7 +1,7 @@
 module "teleport" {
   source = "../../modules/teleport-agent"
 
-  teleport_cluster_name  = local.cluster_name
+  cluster_name           = local.cluster_name
   teleport_proxy_address = var.runtime_teleport_proxy_address
   teleport_auth_token    = var.runtime_teleport_auth_token
 
