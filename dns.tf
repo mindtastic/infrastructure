26e11f653b0e55eebc50a01a2f9864cd5004bf16
isabel <isaschm@users.noreply.github.com>
2022-05-26 12:27:12 +0200

    Fix: Add period to cloudfare record value (#51)
    * Add period to cloudfare record value
    
    * Use tf replace
    
    * Use tf linting suggestion
    
    * Update dns.tf
    
    Co-authored-by: Johannes Gräger <jograeger@googlemail.com>
    
    Co-authored-by: Johannes Gräger <jograeger@googlemail.com>

--- dns.tf
+++ dns.tf
@@ -15,7 +15,7 @@ resource "cloudflare_record" "live_mindtastic_lol" {
 
   zone_id = var.cloudflare_zone_id
   name    = "live"
-  value   = google_dns_managed_zone.live.name_servers[count.index]
+  value   = trimsuffix(google_dns_managed_zone.live.name_servers[count.index], ".")
   type    = "NS"
   ttl     = 3600
 }
