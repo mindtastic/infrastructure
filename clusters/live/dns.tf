1cb3e3f1784b43d81dd11b22a5fea516d94b4cab
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 13:03:12 +0200

    Removed outputs

--- clusters/live/dns.tf
+++ clusters/live/dns.tf
@@ -1,6 +1,6 @@
 # Teleport
 resource "google_dns_record_set" "teleport_internal" {
-  name         = "teleport.net.${google_dns_managed_zone.root.dns_name}"
+  name         = "teleport.net.${data.google_dns_managed_zone.cluster_root.dns_name}"
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.cluster_root.name
--- clusters/live/output.tf
+++ clusters/live/output.tf
@@ -1,12 +1,3 @@
-output "dns_zone_names" {
-  description = "Name Servers for the root DNS zone in this environment"
-  value       = google_dns_managed_zone.root.name_servers
-
-  depends_on = [
-    google_dns_managed_zone.root
-  ]
-}
-
 output "vpc_self_link" {
   description = "Self Link of VPC for this Cluster"
   value       = google_compute_network.vpc_network.self_link
