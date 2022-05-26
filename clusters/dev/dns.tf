f40ec0383b1943707e4c907194ab125260371086
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-05-26 21:25:26 +0200

    Set private cluster endpoints in DNS

--- clusters/dev/dns.tf
+++ clusters/dev/dns.tf
@@ -5,13 +5,3 @@ resource "google_dns_record_set" "kubernetes_internal" {
   managed_zone = data.google_dns_managed_zone.cluster_root.name
   rrdatas      = [google_container_cluster.private_cluster_config.private_endpoint]
 }
-
-# Teleport
-resource "google_dns_record_set" "teleport_internal" {
-  name         = "teleport.net.${data.google_dns_managed_zone.cluster_root.dns_name}"
-  type         = "A"
-  ttl          = 300
-  managed_zone = data.google_dns_managed_zone.cluster_root.name
-  rrdatas      = [module.teleport.teleport_public_ip]
-}
-
