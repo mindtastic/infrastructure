64f2add2c2c430003e53596b91083c837bb98a43
Leo <10501668+ldb@users.noreply.github.com>
2022-05-26 21:36:47 +0200

    Set private cluster endpoints in DNS (#59)
    * Set private cluster endpoints in DNS
    
    * Set private cluster endpoints in DNS
    
    Co-authored-by: Johannes Gräger <jgraeger@posteo.de>
    Co-authored-by: Bengisu Akkir <bengisuakkir@gmail.com>

--- peerings.tf
+++ peerings.tf
@@ -13,12 +13,12 @@ data "google_compute_network" "stage_network" {
 locals {
   # GCP data provider returns an object where google_compute_networks where all
   # string attributes are set to tostring(null) if empty or networks not exist.
-  live_exists  = data.google_compute_network.live_network.self_link != tostring(null)
-  dev_exists   = data.google_compute_network.dev_network.self_link != tostring(null)
-  stage_exists = data.google_compute_network.stage_network.self_link != tostring(null)
+  live_vpc_exists  = data.google_compute_network.live_network.self_link != tostring(null)
+  dev_vpc_exists   = data.google_compute_network.dev_network.self_link != tostring(null)
+  stage_vpc_exists = data.google_compute_network.stage_network.self_link != tostring(null)
 
-  live_stage_exists = local.live_exists && local.stage_exists
-  live_dev_exists   = local.live_exists && local.dev_exists
+  live_stage_exists = local.live_vpc_exists && local.stage_vpc_exists
+  live_dev_exists   = local.live_vpc_exists && local.dev_vpc_exists
 }
 
 resource "google_compute_network_peering" "live_dev_peering" {
