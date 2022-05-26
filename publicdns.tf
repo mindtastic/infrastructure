fea29e0a5cfdce49b0b25eebda4f565bde259e74
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 13:40:26 +0200

    Public DNS for teleport (hardcoded for now)

---
+++ publicdns.tf
@@ -0,0 +1,16 @@
+# Global DNS
+resource "cloudflare_record" "teleport_live_public" {
+  zone_id = var.cloudflare_zone_id
+  name    = "teleport"
+  value   = "teleport.net.live.mindtastic.lol"
+  type    = "CNAME"
+  ttl     = 30
+}
+
+resource "cloudflare_record" "teleport_live_public_wildcard" {
+  zone_id = var.cloudflare_zone_id
+  name    = "*.teleport"
+  value   = "teleport.net.live.mindtastic.lol"
+  type    = "CNAME"
+  ttl     = 30
+}
