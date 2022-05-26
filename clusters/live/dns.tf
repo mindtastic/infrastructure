bf1ae973d59d506e8c9114140f494b874619cd81
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 13:11:04 +0200

    reverted last change

--- clusters/live/dns.tf
+++ clusters/live/dns.tf
@@ -3,6 +3,6 @@ resource "google_dns_record_set" "teleport_internal" {
   name         = "teleport.net.${data.google_dns_managed_zone.cluster_root.dns_name}"
   type         = "A"
   ttl          = 300
-  managed_zone = var.cluster_dns_zone_name
+  managed_zone = data.google_dns_managed_zone.cluster_root.name
   rrdatas      = [module.teleport.teleport_public_ip]
 }
