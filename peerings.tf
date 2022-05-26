1bf1e62cc638abd87c80436e940f45438171f798
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 20:23:47 +0200

    Added peerings in root workspace

---
+++ peerings.tf
@@ -0,0 +1,40 @@
+data "google_compute_network" "live_network" {
+  name = "kubernetes-vpc-live" # Matches clusters/live/vpc.tf
+}
+
+data "google_compute_network" "dev_network" {
+  name = "kubernetes-vpc-dev"
+}
+
+data "google_compute_network" "stage_network" {
+  name = "kubernetes-vpc-stage"
+}
+
+locals {
+  live_stage_exists = data.google_compute_network.live_network.id != "" && data.google_compute_network.stage_network.id != ""
+  live_dev_exists   = data.google_compute_network.live_network.id != "" && data.google_compute_network.dev_network.id != ""
+}
+
+resource "google_compute_network_peering" "live_dev_peering" {
+  count        = local.live_dev_exists ? 1 : 0
+  network      = data.google_compute_network.live_network.self_link
+  peer_network = data.google_compute_network.dev_network.self_link
+}
+
+resource "google_compute_network_peering" "dev_live_peering" {
+  count        = local.live_dev_exists ? 1 : 0
+  network      = data.google_compute_network.dev_network.self_link
+  peer_network = data.google_compute_network.live_network.self_link
+}
+
+resource "google_compute_network_peering" "live_stage_peering" {
+  count        = local.live_stage_exists ? 1 : 0
+  network      = data.google_compute_network.live_network.self_link
+  peer_network = data.google_compute_network.stage_network.self_link
+}
+
+resource "google_compute_network_peering" "stage_live_peering" {
+  count        = local.live_stage_exists ? 1 : 0
+  network      = data.google_compute_network.stage_network.self_link
+  peer_network = data.google_compute_network.live_network.self_link
+}
