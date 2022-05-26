9a97db4fffbc1341852a618962d8b591dcd6d613
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 20:37:13 +0200

    Fixed querieng not existing attributes

--- peerings.tf
+++ peerings.tf
@@ -19,30 +19,30 @@ resource "google_compute_network_peering" "live_dev_peering" {
   count = local.live_dev_exists ? 1 : 0
 
   name         = "live-dev-peering"
-  network      = data.google_compute_network.live_network.self_link
-  peer_network = data.google_compute_network.dev_network.self_link
+  network      = try(data.google_compute_network.live_network.self_link, "")
+  peer_network = try(data.google_compute_network.dev_network.self_link, "")
 }
 
 resource "google_compute_network_peering" "dev_live_peering" {
   count = local.live_dev_exists ? 1 : 0
 
   name         = "dev-live-peering"
-  network      = data.google_compute_network.dev_network.self_link
-  peer_network = data.google_compute_network.live_network.self_link
+  network      = try(data.google_compute_network.dev_network.self_link, "")
+  peer_network = try(data.google_compute_network.live_network.self_link, "")
 }
 
 resource "google_compute_network_peering" "live_stage_peering" {
   count = local.live_stage_exists ? 1 : 0
 
   name         = "live-stage-peering"
-  network      = data.google_compute_network.live_network.self_link
-  peer_network = data.google_compute_network.stage_network.self_link
+  network      = try(data.google_compute_network.live_network.self_link, "")
+  peer_network = try(data.google_compute_network.stage_network.self_link, "")
 }
 
 resource "google_compute_network_peering" "stage_live_peering" {
   count = local.live_stage_exists ? 1 : 0
 
   name         = "stage-live-peering"
-  network      = data.google_compute_network.stage_network.self_link
-  peer_network = data.google_compute_network.live_network.self_link
+  network      = try(data.google_compute_network.stage_network.self_link, "")
+  peer_network = try(data.google_compute_network.live_network.self_link, "")
 }
