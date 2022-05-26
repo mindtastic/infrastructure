64aa7a97e9c24e2a6e73e290b111e7f4b43f9985
Johannes Gräger <jgraeger@posteo.de>
2022-05-26 20:58:25 +0200

    Valid condition for testing for gcp network existence

--- peerings.tf
+++ peerings.tf
@@ -11,40 +11,44 @@ data "google_compute_network" "stage_network" {
 }
 
 locals {
-  live_stage_exists = can(data.google_compute_network.live_network) && can(data.google_compute_network.stage_network)
-  live_dev_exists   = can(data.google_compute_network.live_network) && can(data.google_compute_network.dev_network)
+  # GCP data provider returns an object where google_compute_networks where all
+  # string attributes are set to tostring(null) if empty or networks not exist.
+  live_exists  = data.google_compute_network.live_network.self_link != "null"
+  dev_exists   = data.google_compute_network.dev_network.self_link != "null"
+  stage_exists = data.google_compute_network.stage_network.self_link != "null"
 
-  invalid_fallback_network_uri = "projects/failure/global/networks/fail"
+  live_stage_exists = local.live_exists && local.stage_exists
+  live_dev_exists   = local.live_exists && local.dev_exists
 }
 
 resource "google_compute_network_peering" "live_dev_peering" {
   count = local.live_dev_exists ? 1 : 0
 
   name         = "live-dev-peering"
-  network      = try(data.google_compute_network.live_network.self_link, local.invalid_fallback_network_uri)
-  peer_network = try(data.google_compute_network.dev_network.self_link, local.invalid_fallback_network_uri)
+  network      = data.google_compute_network.live_network.self_link
+  peer_network = data.google_compute_network.dev_network.self_link
 }
 
 resource "google_compute_network_peering" "dev_live_peering" {
   count = local.live_dev_exists ? 1 : 0
 
   name         = "dev-live-peering"
-  network      = try(data.google_compute_network.dev_network.self_link, local.invalid_fallback_network_uri)
-  peer_network = try(data.google_compute_network.live_network.self_link, local.invalid_fallback_network_uri)
+  network      = data.google_compute_network.dev_network.self_link
+  peer_network = data.google_compute_network.live_network.self_link
 }
 
 resource "google_compute_network_peering" "live_stage_peering" {
   count = local.live_stage_exists ? 1 : 0
 
   name         = "live-stage-peering"
-  network      = try(data.google_compute_network.live_network.self_link, local.invalid_fallback_network_uri)
-  peer_network = try(data.google_compute_network.stage_network.self_link, local.invalid_fallback_network_uri)
+  network      = data.google_compute_network.live_network.self_link
+  peer_network = data.google_compute_network.stage_network.self_link
 }
 
 resource "google_compute_network_peering" "stage_live_peering" {
   count = local.live_stage_exists ? 1 : 0
 
   name         = "stage-live-peering"
-  network      = try(data.google_compute_network.stage_network.self_link, local.invalid_fallback_network_uri)
-  peer_network = try(data.google_compute_network.live_network.self_link, local.invalid_fallback_network_uri)
+  network      = data.google_compute_network.stage_network.self_link
+  peer_network = data.google_compute_network.live_network.self_link
 }
