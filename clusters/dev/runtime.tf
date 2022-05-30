d08cadc9e34ae67870f1f2e4f288c6088724eeff
Johannes Gräger <jgraeger@posteo.de>
2022-05-30 23:15:38 +0200

    Create argo-cd service accounts in dev/stage clusters

--- clusters/dev/runtime.tf
+++ clusters/dev/runtime.tf
@@ -11,3 +11,32 @@ module "teleport" {
     google_container_node_pool.primary_preemptible_nodes
   ]
 }
+
+locals {
+  argocd_sa_name      = "argocd-manager"
+  argocd_sa_namespace = "kube-system"
+}
+
+# Create a kubernetes service account for remote cluster management via ArgoCD
+resource "kubernetes_service_account" "argocd_manager" {
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_manager" {
+  metadata {
+    name = "argocd"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
