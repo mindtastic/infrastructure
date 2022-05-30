796feaeb8c494a3af67c8d0112a67da400df47d7
Johannes Gräger <jograeger@googlemail.com>
2022-05-30 15:43:28 +0200

    Update clusters/dev/runtime.tf
    Co-authored-by: Leo <10501668+ldb@users.noreply.github.com>

--- clusters/dev/runtime.tf
+++ clusters/dev/runtime.tf
@@ -4,7 +4,7 @@ module "teleport" {
   cluster_name                 = local.cluster_name
   teleport_proxy_address       = var.runtime_teleport_proxy_address
   teleport_auth_token          = var.runtime_teleport_auth_token
-  kubernetes_environment_label = "dev"
+  kubernetes_environment_label = var.environment
 
   depends_on = [
     google_container_cluster.primary,
