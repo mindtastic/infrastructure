3e32ff7629c26563d99a45ab625cd8c5ca9e37da
Johannes Gräger <jgraeger@posteo.de>
2022-05-31 17:23:48 +0200

    Apply service account credentials to live

--- global-runtime.tf
+++ global-runtime.tf
@@ -28,6 +28,29 @@ locals {
 
   live_stage_cluster_exists = local.live_cluster_exists && local.stage_cluster_exists
   live_dev_cluster_exists   = local.live_cluster_exists && local.dev_cluster_exists
+
+  # Maps clusters/{dev|stage}/runtime.tf
+  argocd_sa_name      = "argocd-manager"
+  argocd_sa_namespace = "kube-system"
+}
+
+data "kubernetes_service_account" "dev_argocd_manager" {
+  count    = local.dev_cluster_exists ? 1 : 0
+  provider = kubernetes.dev_cluster
+
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "dev_argocd_credentials" {
+  count    = local.dev_cluster_exists ? 1 : 0
+  provider = kubernetes.dev_cluster
+
+  metadata {
+    name = data.kubernetes_service_account.dev_argocd_manager.default_secret_name
+  }
 }
 
 # Based on https://argo-cd.readthedocs.io/en/stable/operator-manual/declarative-setup/#clusters
@@ -46,13 +69,36 @@ resource "kubernetes_secret" "argocd_dev_cluster_secret" {
     name   = "dev-cluster",
     server = "https://cluster.net.dev.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.google_client_config.default.access_token,
+      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials.binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.google_container_cluster.dev_cluster.master_auth[0].cluster_ca_certificate,
+        "caData"     = data.kubernetes_secret.dev_argocd_credentials.binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
+  }
+
+  depends_on = [
+    data.kubernetes_secret.dev_argocd_credentials
+  ]
+}
+
+data "kubernetes_service_account" "stage_argocd_manager" {
+  count    = local.stage_cluster_exists ? 1 : 0
+  provider = kubernetes.stage_cluster
+
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "stage_argocd_credentials" {
+  count    = local.stage_cluster_exists ? 1 : 0
+  provider = kubernetes.stage_cluster
+
+  metadata {
+    name = data.kubernetes_service_account.stage_argocd_manager.default_secret_name
   }
 }
 
@@ -71,10 +117,10 @@ resource "kubernetes_secret" "argocd_stage_cluster_secret" {
     name   = "stage-cluster",
     server = "https://cluster.net.stage.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.google_client_config.default.access_token,
+      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials.binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.google_container_cluster.stage_cluster.master_auth[0].cluster_ca_certificate,
+        "caData"     = data.kubernetes_secret.stage_argocd_credentials.binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
