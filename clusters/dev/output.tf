33b2f5f8dae22d82b9b12950d5645f5bc5a5b982
Johannes Gräger <jgraeger@posteo.de>
2022-05-31 17:11:09 +0200

    Removed unneccessary outputs

--- clusters/dev/output.tf
+++ clusters/dev/output.tf
@@ -6,8 +6,3 @@ output "vpc_self_link" {
     google_compute_network.vpc_network
   ]
 }
-
-output "argocd_token" {
-  sensitive = true
-  value     = lookup(data.kubernetes_secret.argocd_token.data, "token")
-}
