mindtastic/infrastructure

commit 1cffaeb8c991c61d4e03be16443cea219206efd4

Create argo-cd service accounts in dev/stage clusters (#67)
* Create argo-cd service accounts in dev/stage clusters

* ArgoCD service account token as module output

* Removed unneccessary outputs

* Kubernetes providers for all clusters in parent workspace

* Apply service account credentials to live

* Fixing counted ressource access

* Fixing provider initialization for non existing kubernetes clusters

clusters/dev/runtime.tf
@@ -11,3 +11,38 @@ module "teleport" {
     google_container_node_pool.primary_preemptible_nodes
   ]
 }
+
+locals {
+  argocd_sa_name      = "argocd-manager"
+  argocd_sa_namespace = "kube-system"
+}
+
+# Create a kubernetes service account for remote cluster management via ArgoCD
+resource "kubernetes_service_account" "argocd_manager" {
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_manager" {
+  metadata {
+    name = "argocd"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "argocd_token" {
+  metadata {
+    name = kubernetes_service_account.argocd_manager.default_secret_name
+  }
+}

clusters/stage/runtime.tf
@@ -11,3 +11,38 @@ module "teleport" {
     google_container_node_pool.primary_preemptible_nodes
   ]
 }
+
+locals {
+  argocd_sa_name      = "argocd-manager"
+  argocd_sa_namespace = "kube-system"
+}
+
+# Create a kubernetes service account for remote cluster management via ArgoCD
+resource "kubernetes_service_account" "argocd_manager" {
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_manager" {
+  metadata {
+    name = "argocd"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "argocd_token" {
+  metadata {
+    name = kubernetes_service_account.argocd_manager.default_secret_name
+  }
+}

global-runtime.tf
@@ -28,6 +28,29 @@ locals {
 
   live_stage_cluster_exists = local.live_cluster_exists && local.stage_cluster_exists
   live_dev_cluster_exists   = local.live_cluster_exists && local.dev_cluster_exists
+
+  # Maps clusters/{dev|stage}/runtime.tf
+  argocd_sa_name      = "argocd-manager"
+  argocd_sa_namespace = "kube-system"
+}
+
+data "kubernetes_service_account" "dev_argocd_manager" {
+  count    = local.dev_cluster_exists ? 1 : 0
+  provider = kubernetes.dev_cluster
+
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "dev_argocd_credentials" {
+  count    = local.dev_cluster_exists ? 1 : 0
+  provider = kubernetes.dev_cluster
+
+  metadata {
+    name = data.kubernetes_service_account.dev_argocd_manager[0].default_secret_name
+  }
 }
 
 # Based on https://argo-cd.readthedocs.io/en/stable/operator-manual/declarative-setup/#clusters
@@ -46,13 +69,36 @@ resource "kubernetes_secret" "argocd_dev_cluster_secret" {
     name   = "dev-cluster",
     server = "https://cluster.net.dev.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.google_client_config.default.access_token,
+      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.google_container_cluster.dev_cluster.master_auth[0].cluster_ca_certificate,
+        "caData"     = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
+  }
+
+  depends_on = [
+    data.kubernetes_secret.dev_argocd_credentials
+  ]
+}
+
+data "kubernetes_service_account" "stage_argocd_manager" {
+  count    = local.stage_cluster_exists ? 1 : 0
+  provider = kubernetes.stage_cluster
+
+  metadata {
+    name      = local.argocd_sa_name
+    namespace = local.argocd_sa_namespace
+  }
+}
+
+data "kubernetes_secret" "stage_argocd_credentials" {
+  count    = local.stage_cluster_exists ? 1 : 0
+  provider = kubernetes.stage_cluster
+
+  metadata {
+    name = data.kubernetes_service_account.stage_argocd_manager[0].default_secret_name
   }
 }
 
@@ -71,10 +117,10 @@ resource "kubernetes_secret" "argocd_stage_cluster_secret" {
     name   = "stage-cluster",
     server = "https://cluster.net.stage.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.google_client_config.default.access_token,
+      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.google_container_cluster.stage_cluster.master_auth[0].cluster_ca_certificate,
+        "caData"     = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })

provider.tf
@@ -46,7 +46,23 @@ provider "random" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.live_cluster.endpoint}"
+  host                   = try("https://${data.google_container_cluster.live_cluster.endpoint}", "")
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.live_cluster.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = try(base64decode(data.google_container_cluster.live_cluster.master_auth[0].cluster_ca_certificate), "")
+}
+
+provider "kubernetes" {
+  alias = "dev_cluster"
+
+  host                   = try("https://${data.google_container_cluster.dev_cluster.endpoint}", "")
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = try(base64decode(data.google_container_cluster.dev_cluster.master_auth[0].cluster_ca_certificate), "")
+}
+
+provider "kubernetes" {
+  alias = "stage_cluster"
+
+  host                   = try("https://${data.google_container_cluster.stage_cluster.endpoint}", "")
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = try(base64decode(data.google_container_cluster.stage_cluster.master_auth[0].cluster_ca_certificate), "")
 }

commit bc75d1cad484bc58e778e9562d4ff701e80ec119

Fixed failing runs after #67 (#68)
* Removed unneccesary data provider

* Debug outputs for secret naem

* More outputs

* Replaced default_secret with secret on data provider

* Remove stage debug outputs

* testing

* testing

* testing

* Hopefully the fix

* Removed debug outputs

* Applying the fix also to stage

Co-authored-by: Leonard Becker <10501668+ldb@users.noreply.github.com>

clusters/dev/.terraform.lock.hcl
@@ -1,0 +1,80 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.20.0"
+  constraints = "4.20.0"
+  hashes = [
+    "h1:jQ0CdHlnQDCogTR8o4wYV7twTiYPOR+si9Ph/UIhpPk=",
+    "zh:0ccae031531ee3d412681b93f2f6941e0e2c43c294d0ea7851a027aa99a32c8a",
+    "zh:1a84e894bf56aa4a7a663a198fd0576cf5a37407e65d2b46bf0c351eb626f7eb",
+    "zh:1c3708268482ad172b08adfc98aa11df5d4cd663d6be4b0ce4bb12c997c5bd49",
+    "zh:2075cb237b3eb90aaae669e4fe41507ce29ed4a4d349fa35f260995f9a73d393",
+    "zh:3ef464693ab8a743e230d0271006b942dc30090640c85efb1c6dfc807837c31f",
+    "zh:62d12548d136a96beded5300cd029e21bf7b89a063aa97422198309d4a7e4c55",
+    "zh:68eefedb6abd2da3fa068abcc9abd5798ff4cce73ff07cdb41a2ccd372e9cb52",
+    "zh:7d5d5e5e8d46a99423ba075064d875903189561fe0658e76c7b305d0c81a1e53",
+    "zh:80f9ccbf7307bc4343ae46defb3cbd572d92490013c16b02deccdb76a606c425",
+    "zh:a9664dc8e1f8a077bd6424afa62334e34fe250c425aa63fa22aaf1cf4a0f6499",
+    "zh:e14be25176dd3b1437107b010e6020f95f3da55be3b0d7c34245fe7a2c630e2e",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/helm" {
+  version     = "2.5.1"
+  constraints = "2.5.1"
+  hashes = [
+    "h1:a9KwjqINdNy6IsEbkHUB1vwvYfy5OJ2VxFL9/NDFLoY=",
+    "zh:140b9748f0ad193a20d69e59d672f3c4eda8a56cede56a92f931bd3af020e2e9",
+    "zh:17ae319466ed6538ad49e011998bb86565fe0e97bc8b9ad7c8dda46a20f90669",
+    "zh:3a8bd723c21ba70e19f0395ed7096fc8e08bfc23366f1c3f06a9107eb37c572c",
+    "zh:3aae3b82adbe6dca52f1a1c8cf51575446e6b0f01f1b1f3b30de578c9af4a933",
+    "zh:3f65221f40148df57d2888e4f31ef3bf430b8c5af41de0db39a2b964e1826d7c",
+    "zh:650c74c4f46f5eb01df11d8392bdb7ebee3bba59ac0721000a6ad731ff0e61e2",
+    "zh:930fb8ab4cd6634472dfd6aa3123f109ef5b32cbe6ef7b4695fae6751353e83f",
+    "zh:ae57cd4b0be4b9ca252bc5d347bc925e35b0ed74d3dcdebf06c11362c1ac3436",
+    "zh:d15b1732a8602b6726eac22628b2f72f72d98b75b9c6aabceec9fd696fda696a",
+    "zh:d730ede1656bd193e2aea5302acec47c4905fe30b96f550196be4a0ed5f41936",
+    "zh:f010d4f9d8cd15936be4df12bf256cb2175ca1dedb728bd3a866c03d2ee7591f",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/http" {
+  version = "2.2.0"
+  hashes = [
+    "h1:xUwT7WL+ImYjm975l/1wu4LAQNKG+V5SIzx/24z2pfM=",
+    "zh:159add5739a597c08439318f67c440a90ce8444a009e7b8aabbcb9279da9191f",
+    "zh:1e5fbe9a4b8d3d9f167effc03bd5324ad6ef721c23a174e98c7eb2e8b85e34e8",
+    "zh:4b150790ac5948ceec4f97df4deaff835e4798049d858c20413cbdff6e610c4d",
+    "zh:4f85c6130249f45ff0dccdcfe78296382c930c288e2f8ec844d73fa48ab3c4ef",
+    "zh:74a1270db30043d9601ed70fecea568693552758f912a37b423dec1530a6f390",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8125231d0b16283130bac7cb4b0c4972a596af10e1a7348ff905dcb02ea61dc9",
+    "zh:83f78277025d276ee6f4fd1ae93cf69b748c6601e7cfc7e30f07beca9ce4dfdd",
+    "zh:abca2c2e14ce0984a1353f03fd13e9dc19312ab7844f64129ec09628e3d5d472",
+    "zh:b5d0f58057c730aab9a0bf348a9143e8a0ae18f2a3ddfb9f56c603aa62212601",
+    "zh:bc6054404263c1d7faaffe4c27d1f93dd5a9848d515a6eae42d43828c3e10447",
+    "zh:cb9d4a0aeebd25cbbae5b7c726deb285c007079191bc43a6a8d6b951b7ef928a",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.11.0"
+  constraints = "2.11.0"
+  hashes = [
+    "h1:T65SZhN/tQgsAsHe/G5PCgpjofi+aTKPZ+nZg6WOJpc=",
+    "zh:143a19dd0ea3b07fc5e3d9231f3c2d01f92894385c98a67327de74c76c715843",
+    "zh:1fc757d209e09c3cf7848e4274daa32408c07743698fbed10ee52a4a479b62b6",
+    "zh:22dfebd0685749c51a8f765d51a1090a259778960ac1cd4f32021a325b2b9b72",
+    "zh:3039b3b76e870cd8fc404cf75a29c66b171c6ba9b6182e131b6ae2ca648ec7c0",
+    "zh:3af0a15562fcab4b5684b18802e0239371b2b8ff9197ed069ff4827f795a002b",
+    "zh:50aaf20336d1296a73315adb66f7687f75bd5c6b1f93a894b95c75cc142810ec",
+    "zh:682064fabff895ec351860b4fe0321290bbbb17c2a410b62c9bea0039400650e",
+    "zh:70ac914d5830b3371a2679d8f77cc20c419a6e12925145afae6c977c8eb90934",
+    "zh:710aa02cccf7b0f3fb50880d6d2a7a8b8c9435248666616844ba71f74648cddc",
+    "zh:88e418118cd5afbdec4984944c7ab36950bf48e8d3e09e090232e55eecfb470b",
+    "zh:9cef159377bf23fa331f8724fdc6ce27ad39a217a4bae6df3b1ca408fc643da6",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}

clusters/dev/runtime.tf
@@ -40,9 +40,3 @@ resource "kubernetes_cluster_role_binding" "argocd_manager" {
     namespace = local.argocd_sa_namespace
   }
 }
-
-data "kubernetes_secret" "argocd_token" {
-  metadata {
-    name = kubernetes_service_account.argocd_manager.default_secret_name
-  }
-}

clusters/stage/runtime.tf
@@ -40,9 +40,3 @@ resource "kubernetes_cluster_role_binding" "argocd_manager" {
     namespace = local.argocd_sa_namespace
   }
 }
-
-data "kubernetes_secret" "argocd_token" {
-  metadata {
-    name = kubernetes_service_account.argocd_manager.default_secret_name
-  }
-}

global-runtime.tf
@@ -49,7 +49,8 @@ data "kubernetes_secret" "dev_argocd_credentials" {
   provider = kubernetes.dev_cluster
 
   metadata {
-    name = data.kubernetes_service_account.dev_argocd_manager[0].default_secret_name
+    name      = data.kubernetes_service_account.dev_argocd_manager[0].default_secret_name
+    namespace = local.argocd_sa_namespace
   }
 }
 
@@ -69,10 +70,10 @@ resource "kubernetes_secret" "argocd_dev_cluster_secret" {
     name   = "dev-cluster",
     server = "https://cluster.net.dev.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["token"],
+      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials[0].data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["ca.crt"],
+        "caData"     = data.kubernetes_secret.dev_argocd_credentials[0].data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
@@ -98,9 +99,11 @@ data "kubernetes_secret" "stage_argocd_credentials" {
   provider = kubernetes.stage_cluster
 
   metadata {
-    name = data.kubernetes_service_account.stage_argocd_manager[0].default_secret_name
+    name      = data.kubernetes_service_account.stage_argocd_manager[0].default_secret_name
+    namespace = local.argocd_sa_namespace
   }
 }
+
 
 resource "kubernetes_secret" "argocd_stage_cluster_secret" {
   count = local.live_stage_cluster_exists ? 1 : 0
@@ -117,10 +120,10 @@ resource "kubernetes_secret" "argocd_stage_cluster_secret" {
     name   = "stage-cluster",
     server = "https://cluster.net.stage.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["token"],
+      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials[0].data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["ca.crt"],
+        "caData"     = data.kubernetes_secret.stage_argocd_credentials[0].data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })