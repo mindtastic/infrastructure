981ed307ec04865c6c7f38b5bbd9ceab71e5b359
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2022-06-12 11:15:13 +0200

    Replaced default_secret with secret on data provider

--- global-runtime.tf
+++ global-runtime.tf
@@ -49,7 +49,7 @@ data "kubernetes_secret" "dev_argocd_credentials" {
   provider = kubernetes.dev_cluster
 
   metadata {
-    name = data.kubernetes_service_account.dev_argocd_manager[0].default_secret_name
+    name = data.kubernetes_service_account.dev_argocd_manager[0].secret[0].name
   }
 }
 
@@ -69,10 +69,10 @@ resource "kubernetes_secret" "argocd_dev_cluster_secret" {
     name   = "dev-cluster",
     server = "https://cluster.net.dev.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials.binary_data["token"],
+      "bearerToken" = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.kubernetes_secret.dev_argocd_credentials.binary_data["ca.crt"],
+        "caData"     = data.kubernetes_secret.dev_argocd_credentials[0].binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
@@ -98,24 +98,24 @@ data "kubernetes_secret" "stage_argocd_credentials" {
   provider = kubernetes.stage_cluster
 
   metadata {
-    name = data.kubernetes_service_account.stage_argocd_manager[0].default_secret_name
+    name = data.kubernetes_service_account.stage_argocd_manager[0].secret[0].name
   }
 }
 
 output "argocd_sa_dev_default_secret_name" {
-  value = data.kubernetes_service_account.dev_argocd_manager.default_secret_name
+  value = data.kubernetes_service_account.dev_argocd_manager[0].default_secret_name
 }
 
 output "argo_sa_dev_secret_name" {
-  value = data.kubernetes_service_account.dev_argocd_manager.secret[0]
+  value = data.kubernetes_service_account.dev_argocd_manager[0].secret[0]
 }
 
 output "argo_sa_stage_secret_name" {
-  value = data.kubernetes_service_account.stage_argocd_manager.secret[0]
+  value = data.kubernetes_service_account.stage_argocd_manager[0].secret[0]
 }
 
 output "argocd_sa_stage_secret_name" {
-  value = data.kubernetes_service_account.stage_argocd_manager.default_secret_name
+  value = data.kubernetes_service_account.stage_argocd_manager[0].default_secret_name
 }
 
 resource "kubernetes_secret" "argocd_stage_cluster_secret" {
@@ -133,10 +133,10 @@ resource "kubernetes_secret" "argocd_stage_cluster_secret" {
     name   = "stage-cluster",
     server = "https://cluster.net.stage.mindtastic.lol",
     config = jsonencode({
-      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials.binary_data["token"],
+      "bearerToken" = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["token"],
       "tlsClientConfig" = {
         "insecure"   = false,
-        "caData"     = data.kubernetes_secret.stage_argocd_credentials.binary_data["ca.crt"],
+        "caData"     = data.kubernetes_secret.stage_argocd_credentials[0].binary_data["ca.crt"],
         "serverName" = "kubernetes.default.svc.cluster.local",
       }
     })
