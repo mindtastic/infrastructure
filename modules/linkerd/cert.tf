09cfd31e06ef50f88b075bab29200c071fd8d782
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-06-23 20:30:16 +0200

    Implement Linkerd

--- modules/linkerd/cert.tf
+++ modules/linkerd/cert.tf
@@ -9,7 +9,6 @@ resource "tls_private_key" "intermediate_ca" {
 }
 
 resource "tls_cert_request" "intermediate_ca" {
-  key_algorithm   = tls_private_key.intermediate_ca.algorithm
   private_key_pem = tls_private_key.intermediate_ca.private_key_pem
 
   subject {
@@ -21,7 +20,6 @@ resource "tls_cert_request" "intermediate_ca" {
 
 resource "tls_locally_signed_cert" "intermediate_ca" {
   cert_request_pem   = tls_cert_request.intermediate_ca.cert_request_pem
-  ca_key_algorithm   = tls_private_key.root_private_key.algorithm
   ca_private_key_pem = tls_private_key.root_private_key.private_key_pem
   ca_cert_pem        = tls_self_signed_cert.root_certificate.cert_pem
 
