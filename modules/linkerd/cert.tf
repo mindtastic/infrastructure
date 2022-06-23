c794b7e7bd9c9edfdfb4f01208cc9189884a707a
Bengisu Akkir <bengisuakkir@gmail.com>
2022-06-23 20:34:45 +0200

    Feat: Implement LinkerD on Dev Cluster (#71)
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    * Implement Linkerd
    
    Co-authored-by: Leo <10501668+ldb@users.noreply.github.com>
    Co-authored-by: Bengisu <bengisuakkir@gmail.com>
    Co-authored-by: isabel <isaschm@users.noreply.github.com>

---
+++ modules/linkerd/cert.tf
@@ -0,0 +1,51 @@
+resource "tls_private_key" "root_private_key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "tls_private_key" "intermediate_ca" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "tls_cert_request" "intermediate_ca" {
+  private_key_pem = tls_private_key.intermediate_ca.private_key_pem
+
+  subject {
+    common_name  = "root.linkerd.cluster.local"
+    organization = "mindtastic"
+  }
+  uris = ["root.linkerd.cluster.local"]
+}
+
+resource "tls_locally_signed_cert" "intermediate_ca" {
+  cert_request_pem   = tls_cert_request.intermediate_ca.cert_request_pem
+  ca_private_key_pem = tls_private_key.root_private_key.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.root_certificate.cert_pem
+
+  validity_period_hours = 8760
+  is_ca_certificate     = true
+  allowed_uses = [
+    "cert_signing",
+    "key_encipherment",
+    "digital_signature",
+  ]
+}
+
+resource "tls_self_signed_cert" "root_certificate" {
+  private_key_pem = tls_private_key.root_private_key.private_key_pem
+
+  subject {
+    common_name  = "root.linkerd.cluster.local"
+    organization = "mindtastic"
+  }
+
+  validity_period_hours = 8760
+  is_ca_certificate     = true
+
+  allowed_uses = [
+    "cert_signing",
+    "key_encipherment",
+    "digital_signature",
+  ]
+}
---
+++ modules/linkerd/helm.tf
@@ -0,0 +1,36 @@
+locals {
+  release_name = "linkerd"
+}
+
+resource "helm_release" "linkerd" {
+  name             = local.release_name
+  repository       = "https://helm.linkerd.io/stable"
+  chart            = "linkerd2"
+  version          = "2.11.2"
+  create_namespace = true # The creates the namespace with some annotations.
+  namespace        = "linkerd"
+  max_history      = 3
+
+  set {
+    name  = "installNamespace"
+    value = false
+  }
+  set_sensitive {
+    name  = "identityTrustAnchorsPEM"
+    value = tls_self_signed_cert.root_certificate.cert_pem
+  }
+  set_sensitive {
+    name  = "identity.issuer.tls.crtPEM"
+    value = tls_locally_signed_cert.intermediate_ca.cert_pem
+  }
+  set_sensitive {
+    name  = "identity.issuer.tls.keyPEM"
+    value = tls_private_key.intermediate_ca.private_key_pem
+  }
+
+  set {
+    name  = "clusterNetworks"
+    type  = "string"
+    value = join("\\,", var.cluster_networks)
+  }
+}
