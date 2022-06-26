9aeeb5f4a61498c10868db59780fcdd25f176a60
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-06-26 19:15:53 +0200

    Allow Teleport to show apps cross cluster

--- modules/teleport-agent/helm.tf
+++ modules/teleport-agent/helm.tf
@@ -33,6 +33,15 @@ resource "helm_release" "teleport" {
   }
 
   set {
+    name = "apps"
+    value = yamlencode([
+      { name : "grafana-${kubernetes_environment_label}", uri : "http://kube-prometheus-stack-grafana:monitoring.svc.cluster.local:3000" },
+      { name : "jaeger-${kubernetes_environment_label}", uri : "http://jaeger.linkerd-viz.svc.cluster.local:16686" },
+      { name : "linkerd-${kubernetes_environment_label}", uri : "http://web.linkerd-viz.svc.cluster.local:8084" },
+    ])
+  }
+
+  set {
     name  = "labels.env"
     value = var.kubernetes_environment_label
   }
