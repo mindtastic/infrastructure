41aec9a2b04dd556aa5b3e62e73d258bec7f9bf2
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-07-10 21:31:17 +0200

    remove linkerd on live

--- clusters/live/runtime.tf
+++ clusters/live/runtime.tf
@@ -37,31 +37,31 @@ module "argo" {
   ]
 }
 
-module "linkerd" {
-  source = "../../modules/linkerd"
+# module "linkerd" {
+#   source = "../../modules/linkerd"
 
-  cluster_networks = [var.k8s_service_subnet, var.k8s_pod_subnet]
+#   cluster_networks = [var.k8s_service_subnet, var.k8s_pod_subnet]
 
-  depends_on = [
-    google_container_cluster.primary,
-    google_container_node_pool.primary_nodes,
-    google_compute_firewall.linkerd
-  ]
-}
+#   depends_on = [
+#     google_container_cluster.primary,
+#     google_container_node_pool.primary_nodes,
+#     google_compute_firewall.linkerd
+#   ]
+# }
 
-# Needed to make Linkerd work on a private GKE cluster.
-# https://github.com/linkerd/linkerd2/issues/8707
-resource "google_compute_firewall" "linkerd" {
-  project     = var.project_name
-  name        = "linkerd-${local.cluster_name}"
-  network     = google_compute_network.vpc_network.self_link
-  description = "Creates firewall rule for linkerd to Kubernetes nodes"
+# # Needed to make Linkerd work on a private GKE cluster.
+# # https://github.com/linkerd/linkerd2/issues/8707
+# resource "google_compute_firewall" "linkerd" {
+#   project     = var.project_name
+#   name        = "linkerd-${local.cluster_name}"
+#   network     = google_compute_network.vpc_network.self_link
+#   description = "Creates firewall rule for linkerd to Kubernetes nodes"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["443", "8443", "8089"] # 8443 for proxy injector, 8089 for `tap`.
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["443", "8443", "8089"] # 8443 for proxy injector, 8089 for `tap`.
+#   }
 
-  source_ranges = [var.k8s_control_plane_subnet]
-  priority      = 0
-}
+#   source_ranges = [var.k8s_control_plane_subnet]
+#   priority      = 0
+# }
